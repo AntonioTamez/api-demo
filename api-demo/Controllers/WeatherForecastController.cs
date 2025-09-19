bfc5c51791598a16c58165e71a7430d64cb7392a
AntonioTamez <antonio.tamez@hotmail.com>
2025-09-18 19:34:33 -0600

    se quita opcion

--- api-demo/Controllers/WeatherForecastController.cs
+++ api-demo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace api_demo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hotest", "Coldest","Scorchingest"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hotest", "Coldest"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
