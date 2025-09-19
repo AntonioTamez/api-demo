56fe7df345fabb0edc623d37b01753547dd12816
AntonioTamez <antonio.tamez@hotmail.com>
2025-09-18 19:38:07 -0600

    se quitan opciones no necesarias

--- api-demo/Controllers/WeatherForecastController.cs
+++ api-demo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace api_demo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hotest", "Coldest"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
